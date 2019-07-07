1fbf1f7b9155309bb41b11cd77ab619191ed0a49
Rodrigo Alfonso <rodri042@gmail.com>
2019-07-07 04:56:43 -0300

    Deploy

---
+++ precache-manifest.f6996da30d0771188207ffc53881dccc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f287531a23d46d22fc93",
+    "url": "/quickp2p/static/js/runtime~main.75399bbd.js"
+  },
+  {
+    "revision": "253d78c28a44f9fa725b",
+    "url": "/quickp2p/static/js/main.de7d405f.chunk.js"
+  },
+  {
+    "revision": "684c0bc58092806275b2",
+    "url": "/quickp2p/static/js/2.6218f2d0.chunk.js"
+  },
+  {
+    "revision": "fce8267e6f148a526cb50fef63110f02",
+    "url": "/quickp2p/index.html"
+  }
+];
